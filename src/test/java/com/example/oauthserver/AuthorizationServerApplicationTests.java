f16363506d4165b6976b363c8eeb9c51c43b2a39
Spyro1001 <raymondhedgecock@gmail.com>
2020-05-11 16:16:34 -0700

    Initial commit

---
+++ src/test/java/com/example/oauthserver/AuthorizationServerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.oauthserver;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class AuthorizationServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+        assertTrue(true);
+    }
+
+}
